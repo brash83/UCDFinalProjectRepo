f3615d69f7c6819c7c1e0ee0552af6203bf2883c
Brash83 <briancashjr@gmail.com>
2025-01-16 21:19:50 +0000

    Updated styles, navbar, added new Avatar component and project images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    publicDir: 'public', // Ensures the correct folder is used for static files like styles.css
+  },
+});
