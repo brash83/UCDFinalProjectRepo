f07d916a798213e97f39bebe8aee3725d8b3bccc
Brash83 <briancashjr@gmail.com>
2025-02-06 10:22:13 +0000

    Deploy Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  vite: {
-    publicDir: 'public', // Ensures the correct folder is used for static files like styles.css
-  },
+  site: 'https://brash83.github.io/UCDFinalProjectRepo',
 });
