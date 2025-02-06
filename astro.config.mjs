a35c5b141e6185a7b5de9f2e91180934e20836c2
Brash83 <briancashjr@gmail.com>
2025-02-06 10:27:24 +0000

    Update site with Astro build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://brash83.github.io/UCDFinalProjectRepo',
+  build: {
+    out: 'docs', // Set the build folder to docs for GitHub Pages deployment
+  },
 });
