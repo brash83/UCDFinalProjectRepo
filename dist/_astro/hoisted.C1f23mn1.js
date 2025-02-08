41f6765580d7a3073821a4b56fd35d21b9084552
Brash83 <briancashjr@gmail.com>
2025-02-08 19:05:06 +0000

    Deploy updated site

--- dist/_astro/hoisted.C1f23mn1.js
+++
@@ -1,1 +0,0 @@
-const t=document.getElementById("mode-toggle");if(t){let e=localStorage.getItem("dark-mode")==="true";t.textContent=e?"🌙":"🌞",t.addEventListener("click",()=>{e=!e,t.textContent=e?"🌙":"🌞",document.body.classList.toggle("dark-mode",e),localStorage.setItem("dark-mode",e.toString())})}
