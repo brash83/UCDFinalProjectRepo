a676086341ae0d88ca40bb459bc6b044f2a7d7cd
Brash83 <briancashjr@gmail.com>
2025-02-08 16:58:57 +0000

    Updated layout with more margin and add hamburger for mobile use

---
+++ .history/astro.config_20250207202616.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://brash83.github.io/UCDFinalProjectRepo',
+  build: {
+    outDir: 'dist',  // ✅ Keep output in 'dist/' instead of 'docs/'
+  },
+  base: '/UCDFinalProjectRepo/',
+});
